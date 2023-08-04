1e0ba1e234435222e0f0227f4629d6fee355a0e4
Shohan Sharoar <maffin.killer007@gmail.com>
2023-08-04 22:16:59 +0600

    Initial files and router setup.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import Home from './components/Home'
+import router from './utils/routes'
 
 function App() {
+  
   return (
     <div className='flex min-h-screen justify-center items-center'>
-      <h1 className='text-5xl'>Welcome to Smart Home Starter Template</h1>
+      <RouterProvider router={ router } />
     </div>
   )
 }
---
+++ src/utils/routes.js
@@ -0,0 +1,30 @@
+import { Children } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../components/Home";
+import Root from "../components/Root";
+import About from '../components/About';
+import ErrorPage from '../components/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/home',
+        element: <Home />
+      },
+      {
+        path: '/about',
+        element: <About />
+      }
+    ]
+  }
+])
+
+export default router
