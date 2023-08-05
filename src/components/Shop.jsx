f329bf88f4dbe376e652f9dabcdba0ef6b7b7926
Shohan Sharoar <maffin.killer007@gmail.com>
2023-08-05 17:16:46 +0600

    Add To cart functionality added with an event handler ino Add To Cart Button

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,13 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { ProductContext } from './Root'
 import Product from './Product'
+import { addToDb } from '../utils/fakeDB';
+import { toast } from 'react-toastify';
 
 const Shop = () => {
   // Getting the product through Context API
   const products = useContext( ProductContext );
+  // Using a state for Cart Calculation
+  const [ cart, setCart ] = useState([]);
+  console.log( cart );
+
   // Add To Cart Event Handler
   const handleAddToCart = product => {
-    console.log( product )
+    let newCart = [];
+
+    // finding if product already exist or not. If not then we'll add product quantity to 1
+    const existingProduct = cart.find( existingProduct => existingProduct.id === product.id )
+
+    if( !existingProduct ){
+      product.quantity = 1;
+      newCart = [ ...cart, product ];
+    }
+    else{
+      const restProducts = cart.filter( existingProduct => existingProduct.id !== product.id )
+
+      existingProduct.quantity = existingProduct.quantity + 1;
+      newCart = [...cart, existingProduct];
+    }
+    setCart( newCart );
+    addToDb( product.id );
+    toast.success( 'Product Added Successfully!🥳', { autoClose: 500 } )
   }
   return (
     <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
