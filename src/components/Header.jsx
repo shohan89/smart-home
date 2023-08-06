6bcd151e1a2bf756fdedcd0975cdf8673a848f70
Shohan Sharoar <maffin.killer007@gmail.com>
2023-08-06 17:02:45 +0600

    Update Cart and finish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { CartContext } from './Root'
 
 const Header = () => {
+  const [ cart, setCart ] = useContext( CartContext );
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   return (
     <div className='px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
@@ -52,7 +54,10 @@ const Header = () => {
               title='Cart'
               className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
             >
-              <p> Cart</p>
+              <div className='relative py-3'>
+                <p>Cart</p>
+                <p className='absolute bottom-5 left-9'>{ cart.length }</p>
+              </div>
             </Link>
           </li>
           <li>
@@ -147,7 +152,10 @@ const Header = () => {
                         title='Cart'
                         className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
                       >
-                        <p> Cart</p>
+                        <div className='relative py-3'>
+                          <p>Cart</p>
+                          <p className='absolute bottom-5 left-9'>{ cart.length }</p>
+                        </div>
                       </Link>
                     </li>
                     <li>
